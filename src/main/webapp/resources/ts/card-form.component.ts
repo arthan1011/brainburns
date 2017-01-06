58dd6eb73e59f215fdda1a91fa4ab1115159278c
Artur Shamsiev <arthan1011@gmail.com>
2017-01-06 22:35:03 +0500

    new card form layout

--- src/main/webapp/resources/ts/card-form.component.ts
+++ src/main/webapp/resources/ts/card-form.component.ts
@@ -7,6 +7,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: "bb-card-form",
     moduleId: module.id,
+    styleUrls: ["../css/card-form.component.css"],
     templateUrl: "../template/card-form.component.html"
 })
 export class CardFormComponent {
