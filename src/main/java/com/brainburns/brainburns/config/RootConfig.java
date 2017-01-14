3ab5f33912e4c759f9fb73cac066d50084fbdd75
arthan1011 <arthan1011@gmail.com>
2017-01-14 21:32:37 +0300

    database authentication

--- src/main/java/com/brainburns/brainburns/config/RootConfig.java
+++ src/main/java/com/brainburns/brainburns/config/RootConfig.java
@@ -1,15 +1,65 @@
 package com.brainburns.brainburns.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.MessageFormat;
 
 /**
  * Created by Arthan on 24.12.2016. Project: brainburns
  */
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages = "com.brainburns.brainburns")
 public class RootConfig {
 
+    private DataSource dataSource;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        if (this.dataSource == null) {
+            final String dbUrlTemplate = "jdbc:postgresql://{0}:{1}{2}";
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.postgresql.Driver");
+
+            String username;
+            String password;
+            String url;
+
+            String databaseUrl = System.getenv("DATABASE_URL");
+            if (databaseUrl != null) {
+                URI dbUri = new URI(databaseUrl);
+                username = dbUri.getUserInfo().split(":")[0];
+                password = dbUri.getUserInfo().split(":")[1];
+                url = MessageFormat.format(dbUrlTemplate, dbUri.getHost(), dbUri.getPort(), dbUri, dbUri.getPath());
+            } else {
+                username = "postgres";
+                password = "postgres";
+                url = "jdbc:postgresql://localhost:5432/brainburns_local";
+            }
+
+            dataSource.setUrl(url);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+
+            this.dataSource = dataSource;
+        }
+
+        return this.dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws URISyntaxException {
+        return new DataSourceTransactionManager(dataSource());
+    }
 }
--- src/main/java/com/brainburns/brainburns/config/SecurityConfig.java
+++ src/main/java/com/brainburns/brainburns/config/SecurityConfig.java
@@ -6,15 +6,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 /**
  * Created by arthan on 12.01.2017. | Project brainburns
  */
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("arthan").password("qazqaz").roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
     }
 
     @Override
