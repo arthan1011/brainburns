720e97cff6a05e7ead50ef234c5c9d9e9d80c95c
arthan1011 <arthan1011@gmail.com>
2017-01-23 22:54:53 +0300

    brainburns-7 Move "New desk" button to desk-list component.

--- src/main/webapp/resources/app/desks/desk-list.component.ts
+++ src/main/webapp/resources/app/desks/desk-list.component.ts
@@ -2,7 +2,7 @@
  * Created by arthan on 17.01.2017.
  */
 
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, EventEmitter, Output} from "@angular/core";
 import {Desk} from "./model/Desk";
 import {Router} from "@angular/router";
 import {DeskService} from "./desk.service";
@@ -15,6 +15,10 @@ import {DeskService} from "./desk.service";
 })
 export class DeskListComponent implements OnInit {
 
+    public desks: Desk[];
+    @Output()
+    public onNewDesk: EventEmitter<any> = new EventEmitter();
+
     constructor(
         private deskService: DeskService,
         private router: Router
@@ -25,7 +29,9 @@ export class DeskListComponent implements OnInit {
             .subscribe(desks => this.desks = desks);
     }
 
-    public desks: Desk[];
+    newDesk() {
+        this.onNewDesk.emit();
+    }
 
     selectDesk(desk: Desk): void {
         this.router.navigate(["/desks", desk.id]);
