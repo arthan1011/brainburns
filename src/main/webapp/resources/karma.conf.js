cdb5231668623ed130a5514c880e484fc08cc798
arthan1011 <arthan1011@gmail.com>
2017-02-20 07:57:50 +0300

    test attempt 1

--- src/main/webapp/resources/karma.conf.js
+++ src/main/webapp/resources/karma.conf.js
@@ -5,11 +5,27 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine'],
+        frameworks: ['systemjs', 'jasmine'],
 
         browsers: ['Chrome'],
 
         files: [
+            'node_modules/core-js/client/shim.min.js',
+            'node_modules/reflect-metadata/Reflect.js',
+            'node_modules/zone.js/dist/zone.js',
+            'node_modules/systemjs/dist/system.src.js',
+
+            {pattern: 'systemjs.config.js', included: false, watcher: false},
+
+            'karma-test-shim.js',
+
+            {pattern: 'app/**/*.js', included: false, watcher: false},
+            {pattern: 'app/**/*.js.map', included: false, watcher: false},
+            {pattern: 'app/**/*.html', included: false, watcher: false},
+            {pattern: 'app/**/*.css', included: false, watcher: false},
+            { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
+            { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
+
             'app/first-test.spec.js',
             'app/app.component.spec.js'
         ],
@@ -17,6 +33,7 @@ module.exports = function (config) {
         // Karma plugins loaded
         plugins: [
             'karma-jasmine',
+            'karma-systemjs',
             'karma-chrome-launcher'
         ],
 
--- src/main/webapp/resources/systemjs.config.js
+++ src/main/webapp/resources/systemjs.config.js
@@ -6,12 +6,12 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'resources/node_modules/'
+      'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'resources',
+      app: '',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -26,6 +26,9 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+
+        // for tests
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
