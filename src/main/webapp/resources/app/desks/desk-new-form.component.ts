7f094cb42a780e4bd6f1bcca8b93c8e8e8b2e6e7
arthan1011 <arthan1011@gmail.com>
2017-03-13 00:33:55 +0300

    brainburns-15 Clear new desk form after new desk created

--- src/main/webapp/resources/app/desks/desk-new-form.component.ts
+++ src/main/webapp/resources/app/desks/desk-new-form.component.ts
@@ -32,5 +32,6 @@ export class DeskNewFormComponent {
                 () => this.deskCommunicationService.deskCreated("success"),
                 (error: Response) => this.notificationService.error(error.json().message)
             );
+        this.desk = new Desk();
     }
 }
